717b9af65abc4adf983c6172bc26e22114d352ed
goutham4l <angalagouthamkumar@gmail.com>
2026-03-08 14:51:55 +0530

    switch database to sqlite

--- prediction_of_air_pollution/settings.py
+++ prediction_of_air_pollution/settings.py
@@ -67,14 +67,20 @@ WSGI_APPLICATION = 'prediction_of_air_pollution.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'prediction_of_air_pollution',
+#         'USER':'root',
+#         'PASSWORD': '',
+#         'HOST' :'127.0.0.1',
+#         'PORT' :'3306',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'prediction_of_air_pollution',
-        'USER':'root',
-        'PASSWORD': '',
-        'HOST' :'127.0.0.1',
-        'PORT' :'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
